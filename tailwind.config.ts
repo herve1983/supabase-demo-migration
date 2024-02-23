6a9173916d46ae9659d8ab8bad62f7d633797d1b
herve1983 <87281704+herve1983@users.noreply.github.com>
2024-02-23 03:33:11 +0100

    Develop (#6)
    * Install NextUI
    
    * Install eslint-config-prettier
    
    * Install prettier and format project (run the script -> prettier:write )

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+import { nextui } from "@nextui-org/react";
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -5,11 +7,11 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [nextui()],
 };
 export default config;
