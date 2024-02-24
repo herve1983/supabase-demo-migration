d6d3674570327391e3b4005cea27ba005e2bd825
herve1983 <87281704+herve1983@users.noreply.github.com>
2024-02-24 07:59:48 +0100

    Add enum roomType

--- global.d.ts
+++ global.d.ts
@@ -2,5 +2,6 @@ import type { Database } from "./types.gen";
 
 declare global {
   type users = Database["public"]["Tables"]["users"]["Row"];
-  ty;
+  type room = Database["public"]["Tables"]["room"]["Row"];
+  type roomType = Database["public"]["Enums"]["room_type"];
 }
