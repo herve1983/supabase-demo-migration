9677134f8601f3e31cc896e2ea677a5f55fcbd46
herve1983 <87281704+herve1983@users.noreply.github.com>
2024-02-24 08:52:27 +0100

    Delete type(ENUM in PostgreSQL) room_type

--- global.d.ts
+++ global.d.ts
@@ -3,5 +3,4 @@ import type { Database } from "./types.gen";
 declare global {
   type users = Database["public"]["Tables"]["users"]["Row"];
   type room = Database["public"]["Tables"]["room"]["Row"];
-  type roomType = Database["public"]["Enums"]["room_type"];
 }
